0cb7139fb35e4c3b2c56c82a86e8ccf22455645f
godlike34 <3095700877@example.com>
2026-06-01 13:50:11 +0800

    Add /about route

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello():
     return "<h1>Hello — Day 1 完成</h1><p>Flask 项目已启动。</p>"
 
+@app.route("/about")
+def about():
+    return "<h1>关于这个项目</h1><p>个人任务管理系统，我在用 Flask 学习后端。</p>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
